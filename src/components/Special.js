3b8f5bf49ffb0c447b751b2dc19c508d3cf17630
Agatha <master369_@hotmail.com>
2023-03-10 12:25:32 +0000

    remove btn in Special and modify reservation btn

--- src/components/Special.js
+++ src/components/Special.js
@@ -8,15 +8,11 @@ const Special = () => {
       <div className={`container`}>
         <div className={styles.title}>
           <h2 className="sub_title">This week Special menu </h2>
-          <button className='primary-btn'>Online Menu</button>
         </div>
         <div className={styles.cards}>
           {specialMenu.map((specialItem) => (
             <SpecialCard specialItem={specialItem} key={specialItem.id} />
           ))}
-          {/* <SpecialCard />
-          <SpecialCard />
-          <SpecialCard /> */}
         </div>
       </div>
     </section>
